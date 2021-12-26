f8c3acd3dc2a100a842b6e6c3c776e163b205518
Mr-Gradus <rybin.dmitriy@gmail.com>
2021-12-26 15:58:14 +0500

    Task 2 is done (probable)

--- Source.cpp
+++ Source.cpp
@@ -15,12 +15,33 @@ T div(T x, T y)
 }
 
 
-
-
 //#################### TASK_2 ####################
 
+class Ex {
+//private:
+	//double x;
+public:
+	double x;
+	Ex(double) {}
+	Ex() {}
+	~Ex() {}
+};
 
-
+class Bar {
+private:
+	double y = 0;
+public:
+	Bar(double) {}
+	Bar() {}
+	~Bar() {}
+	
+	void setBar(const double &a)
+	{
+		if ((y + a) > 100)
+			throw Ex(a * y);
+		else (y = a);
+	}
+};
 
 int main()
 {
@@ -37,11 +58,23 @@ int main()
 		cerr << ex << endl;
 	}
 
-
 	cout << endl << "#################### TASK_2 ####################" << endl << endl;
 
+	Bar bar;
+	int n;
+	while (cin >> n && n != 0)
+	{
+		try
+		{
+			bar.setBar(n);
+		}
 
+		catch (Ex& c)
+		{
 
+			cerr << "Error: " << c.x << endl;
+		}
+	}
 	return 0;
 
 }
